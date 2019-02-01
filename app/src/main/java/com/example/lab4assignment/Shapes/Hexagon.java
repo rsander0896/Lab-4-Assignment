3c944424a862650ff0855db9750904231fd8ed9b
jusong <jusong@ualberta.ca>
2019-01-31 18:33:10 -0700

    added Hexagon

--- app/src/main/java/com/example/lab4assignment/Shapes/Hexagon.java
+++ app/src/main/java/com/example/lab4assignment/Shapes/Hexagon.java
@@ -1,4 +1,13 @@
 package com.example.lab4assignment.Shapes;
 
-public class Hexagon {
+public class Hexagon extends Shape{
+    private int x;
+    private int y;
+
+    public Hexagon(int x, int y) {
+        super();
+
+        this.x = x;
+        this.y = y;
+    }
 }
